25e1320b9667b5a2c24bb6e09c5b22a9fc06fe59
Zerberstian <248627354+Zerberstian@users.noreply.github.com>
2025-12-12 13:22:43 +0100

    Add RAM class with initialization from RAM_Riegel

--- RAM-Class.py
+++ RAM-Class.py
@@ -3,3 +3,8 @@ import json
 with open('RAM.json', 'r', encoding='utf-8') as f:
   RAM_Riegel = json.load(f)
 
+class RAM:
+  def __init__(self, name):
+    self.name = name
+    for i in RAM_Riegel:
+      if i 
